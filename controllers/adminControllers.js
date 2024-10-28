27da30a57d07e278747462b20c7fb8a06297220f
nacho <nachogbd@hotmail.com>
2024-10-28 21:58:28 +0100

    correciones de la maquetacion

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -1,9 +1,116 @@
+
+const { consulta } = require("../helpers/ajax");
+
+
 /* get Admin */
-const getAdmin = (req, res) =>{
-    console.log('accediendo a admin')
-    res.render('/index',{
-   
+const getAdmin = (req, res) => {
+    res.render('admin/movies', {
     })
 }
+/**
+ * 
+ * @param {*Object} req donde se almacena el request de la funcion
+ * @param {*Object} res donde se alamacena la respuesta de la funcion
+ * Nos devuelve el estado de la respuesta
+ */
 
-module.exports = {getAdmin}
+const getAllMovies = async (req, res) => {
+    const url = "movies";
+    try {
+        const { ok, data, msg } = await consulta(url)
+        res.render('admin/movies', {
+            ok,
+            data,
+            msg: 'Peliculas'
+        })
+
+    } catch (error) {
+        throw (error)
+    }
+
+}
+
+const getMovieByTitle = async (req, res) => {
+        const title = req.query.title;
+        if (!title) {
+            return res.status(400).send('Se debe proporcionar un título de película.');
+        }
+
+        const url = 'movies';
+
+        try {
+            const { ok, data, msg } = await consulta(url);
+
+
+            if (!ok) {
+                return res.status(404).send('No se encontraron películas');
+            }
+
+            res.render('admin/movies', {
+                ok,
+                data,
+                msg: 'Resultados de búsqueda para: ' + title
+            });
+
+        } catch (error) {
+            res.status(500).send('Error al buscar la película');
+        }
+    };
+
+const createMovies = async (req, res) => {
+    const { pelicula, año, director, genero, duracion } = req.body
+    const body = {
+        pelicula, año, director, genero, duracion
+    }
+    try {
+        await consulta('movies', 'post', body)
+
+    } catch (error) {
+        console.log(error)
+    }
+    res.redirect('/admin/movies')
+}
+
+const updateMovies = async (req, res) => {
+    try {
+        const movies = await consulta.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+        console.log(movies)
+        if (!movies) {
+            return res.status(404).json({
+                msg: "pelicula no encontrada",
+
+            })
+        }
+        res.status(200).json({
+            ok: true,
+            msg: "La pelicula ha sido modificada",
+            movies
+        })
+
+    } catch (error) {
+        throw (error)
+
+    }
+}
+
+const deleteMovies = async (req, res) => {
+    try {
+        const deleteMovies = await consulta.findByIdAndDelete(id);
+        if (!deleteMovies) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'pelicula no encontrada'
+            });
+        }
+        res.status(200).json({
+            ok: true,
+            msg: 'pelicula eliminada correctamente',
+            deleteMovies,
+        });
+    } catch (error) {
+        throw (error)
+
+    }
+}
+
+module.exports = { getAdmin, getAllMovies, getMovieByTitle, createMovies, updateMovies, deleteMovies }
--- routers/adminRouters.js
+++ routers/adminRouters.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router()
 
-const {getAdmin}= require('../controllers/adminControllers')
+const { getAdmin,
+        getAllMovies,
+        getMovieByTitle,
+        createMovies,
+        updateMovies,
+        deleteMovies
+     } = require('../controllers/adminControllers')
 
 /* get Admin*/
-router.get('/admin',getAdmin)
+router.get('/admin', getAdmin)
+router.get('/admin/movies', getAllMovies)
+router.get('/admin/movies', getMovieByTitle)
+router.post('/admin/createMovies', createMovies)
+router.put('/admin/editMovies', updateMovies)
+router.delete('/admin/editMovies', deleteMovies)
+
 
 
 module.exports = router;
