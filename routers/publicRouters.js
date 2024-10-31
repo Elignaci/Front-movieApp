65a15787bfc1f29b1531b7aa8e27c9db9804eed4
EliasBM98 <elias.ben8ez@gmail.com>
2024-10-31 12:47:53 +0100

    ruta todas las peliculas user

--- routers/publicRouters.js
+++ routers/publicRouters.js
@@ -4,14 +4,17 @@ const router = express.Router()
 const {
     getDashboard,
     getMovies,
-    searchMovies
+    searchMovies,
+    searchMoviesByTitle
     
-    }= require('../controllers/publicControllers')
+    }= require('../controllers/publicControllers');
+
 
 /* get index */
-router.get('/dashboard',getDashboard)
-router.get('/movies',getMovies)
-router.get('/search',searchMovies)
+router.get('/dashboard', getDashboard);
+router.get('/movies', getMovies);
+router.get('/search', searchMovies);
+router.post('/searchtitle', searchMoviesByTitle);
 
 
 
